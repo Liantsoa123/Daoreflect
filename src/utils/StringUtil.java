beb4ffd71a9ee30ea31bc6fe54e7009413dce8a2
Liantsoa123 <rakotonanaharyliantsoa@gmail.com>
2024-06-04 22:57:29 +0300

    create class StringUtil

---
+++ src/utils/StringUtil.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class StringUtil {
+    public static String toUppurcaseFirstLetter(String str) {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+}
