686565e8c7076ceaf56a68528fda75435f06e3e9
Liantsoa123 <rakotonanaharyliantsoa@gmail.com>
2024-06-04 22:28:07 +0300

    create annotation class

---
+++ src/annotation/AnnotationClass.java
@@ -0,0 +1,11 @@
+package annotation;
+
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AnnotationClass {
+    String value();
+    
+}
+
