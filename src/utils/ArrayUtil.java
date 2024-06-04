0d95c4350b4c298c2873dfb1361b31b02cc67e5c
Liantsoa123 <rakotonanaharyliantsoa@gmail.com>
2024-06-04 22:55:36 +0300

    creat utils ArrayUtil

---
+++ src/utils/ArrayUtil.java
@@ -0,0 +1,41 @@
+package utils;
+
+import java.util.Vector;
+
+public class ArrayUtils {
+    public static String implodeStr(String[] arrayStrings, String separator) {
+        if (arrayStrings != null || arrayStrings.length != 0) {
+            String value = "";
+            for (int i = 0; i < arrayStrings.length; i++) {
+                value += arrayStrings[i];
+                if (i != arrayStrings.length - 1) {
+                    value += separator;
+                }
+            }
+            return value;
+        }
+        return null;
+    }
+
+    public static String implodeValues(Vector vector, String separator) {
+        if (vector != null || vector.size() == 0) {
+            String value = "";
+            for (int i = 0; i < vector.size(); i++) {
+                if (vector.get(i).getClass().equals(Integer.TYPE) || vector.get(i).getClass().equals(Double.TYPE)
+                        || vector.get(i).getClass().equals(Float.TYPE)) {
+                    value += vector.get(i);
+                } else {
+                    value += "'" + vector.get(i) + "'";
+                }
+                if (i != vector.size() - 1) {
+                    value += separator;
+                }
+            }
+            return value;
+        }
+        return null;
+    }
+
+    
+
+}
