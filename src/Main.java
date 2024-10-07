2638d47f11c695238c05b0d26602b5e654232091
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-10-07 14:00:21 +0300

    fixe the function get connection to enable in oracle database

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
+import java.sql.Connection;
+
 import mg.dao.DaoReflect;
 
 public class Main {
     public static void main(String[] args) {
         try {
             DaoReflect daoReflect = new DaoReflect(
-                    "D:\\IT_University\\IT University\\S4\\Web Dyn (Mr Naina)\\DaoReflect\\Daoreflect\\conf.xml");
-            System.out.println(daoReflect.getUtilDb().getDriver());
+                    "C:\\Users\\rakot\\OneDrive\\Documents\\S5\\Zavatra ilaina\\Reflect\\Daoreflect\\Dao.xml");
+            Connection c = daoReflect.getUtilDb().getCon();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
+/*
+    Dao.xml exemple for oracle
+ * <dao>
+ * <url>jdbc:oracle:thin:@localhost:1521:orcl</url>
+ * <database>GestionAnalytique</database>
+ * <user>StationNew</user>
+ * <password>liantsoa</password>
+ * <bdd>oracle</bdd>
+ * </dao>
+ */
